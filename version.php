911e8cb29290ab8832da9eb788708dc993364ddb
Joseph Rezeau <joseph@rezeau.org>
2012-01-13 18:31:58 +0100

    made version.php upward compliant with moodle 2.2
    Signed-off-by: Joseph Rezeau <joseph@rezeau.org>

--- version.php
+++ version.php
@@ -1,6 +1,34 @@
-<?PHP
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-$plugin->version  = 2012011000;
+/**
+ * Version information for the calculated question type.
+ *
+ * @package    qbehaviour
+ * @subpackage regexpadaptivewithhelp
+ * @copyright  2011-2012 Joseph Rézeau
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'qbehaviour_regexpadaptivewithhelp';
+$plugin->version  = 2012010100;
+
 $plugin->requires = 2011070102.03;
 
-?>
+$plugin->maturity  = MATURITY_STABLE;
+
