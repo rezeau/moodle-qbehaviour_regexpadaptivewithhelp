eb6d81fd16d36b4cc8989f5499ae434ccea26bb4
rezeau <joseph@rezeau.org>
2019-05-29 19:26:15 +0200

    fixed required moodle version (3.1)

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qbehaviour_regexpadaptivewithhelp';
 $plugin->version  = 2019052100;
-$plugin->requires = 2019052000; // Moodle version.
+$plugin->requires = 2016052300; // Moodle version.
 $plugin->release = '3.7.0 for Moodle 3.1-3.7';
 $plugin->maturity  = MATURITY_STABLE;
 
